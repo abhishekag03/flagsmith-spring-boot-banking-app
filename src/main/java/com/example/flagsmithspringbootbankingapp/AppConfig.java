f0515e1360397857aef48f48b82b67167380b38b
Abhishek Agarwal <abhishekag03@gmail.com>
2023-11-20 22:18:34 +0530

    add wiring for dependency injection (#3)
    Co-authored-by: Abhishek Agarwal <abhihsekag03@gmail.com>

---
+++ src/main/java/com/example/flagsmithspringbootbankingapp/AppConfig.java
@@ -0,0 +1,21 @@
+package com.example.flagsmithspringbootbankingapp;
+
+import com.flagsmith.FlagsmithClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public AccountService accountService() {
+        return new AccountService();
+    }
+
+    @Bean
+    public FlagsmithClient flagsmithClient() {
+        return FlagsmithClient
+                .newBuilder()
+                .setApiKey("ser.9ZHAnRhWWxs3xschtnbp6Z")
+                .build();
+    }
+}
